93c934c0c5d218b576c228a4e20e9c76bf3d5843
Selvam2004 <143206715+Selvam2004@users.noreply.github.com>
2024-02-15 12:02:38 +0530

    Update dashboard.js

--- src/admin/dashboard.js
+++ src/admin/dashboard.js
@@ -7,7 +7,7 @@ export default function Dashboard(){
     const navigate=useNavigate(); 
     axios.defaults.withCredentials=true;
     useEffect(()=>{
-        axios.get("https://tripadvisor-api.onrender.com/dashboard")
+        axios.get("http://localhost:3000/dashboard")
         .then((res)=>{
             if(res.data==="success"){ 
                console.log("welcome admin");
