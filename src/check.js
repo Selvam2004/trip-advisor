e487d5c4b8e927014db25d920dd5905d01fec929
Selvam2004 <selvam032004@gmail.com>
2024-02-16 23:17:21 +0530

    0.0.0.1

--- src/check.js
+++ src/check.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-
-<<<<<<< HEAD
+ 
 export default function Check() {
   const navigate = useNavigate();
   axios.defaults.withCredentials = true;
@@ -25,27 +24,4 @@ export default function Check() {
       <Outlet />
     </>
   );
-=======
-export default function Check(){
-    const navigate=useNavigate(); 
-    axios.defaults.withCredentials=true;
-    useEffect(()=>{
-        axios.get("https://tripadvisor-api.onrender.com/dashboard")
-        .then((res)=>{
-            if(res.data==="user"){ 
-                console.log("welcome to Trip Advisor");
-            }
-            else{
-                console.log(res.data)
-                navigate('/'); 
-            }
-        }).catch(err=>console.log(err));
-        // eslint-disable-next-line
-        },[]);
-    return (
-        <>
-        <Outlet/>  
-        </>
-    );
->>>>>>> 1bbaf6a52c9e0205856431b49732b5c733cb4c25
-}
+ 
