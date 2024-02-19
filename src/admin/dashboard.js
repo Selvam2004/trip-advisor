374805c16d0d58e6f7f070446112af921c71f47e
Selvam2004 <selvam032004@gmail.com>
2024-02-19 10:36:39 +0530

    0.0.0.1

--- src/admin/dashboard.js
+++ src/admin/dashboard.js
@@ -25,7 +25,7 @@ export default function Dashboard() {
             .finally(() => {
                 setLoading(false);
             });
-    }, [navigate]);
+    }, []);
 
     if (loading) {
         return <div>Loading...</div>;
